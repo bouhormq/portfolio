51037e95d4a591602365b746c014b86ae392a9c6
Salim Bouhorma <67782664+bouhormq@users.noreply.github.com>
2020-12-21 19:25:38 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,6 +17,7 @@ class App extends Component{
   }
 
   componentDidMount = () => {
+    document.title = "Salim Bouhorma Mouffak"
     setTimeout(() => {
       this.setState({
         loading:false
