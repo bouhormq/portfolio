87d3167a81f87f9aa4c6bced9fe472ee63a25a3d
bouhormq <bouhormq@gmail.com>
2021-02-02 23:05:19 +0100

    safari

--- src/components/videoplayer.js
+++ src/components/videoplayer.js
@@ -1,11 +1,40 @@
 import React, { useRef, useState, useEffect } from "react";
 import mainVideo from "../media/corsa.mp4"
+import gifvideo from "../media/corsa.gif"
+import PhoneScreenStyles from "../styles/phonescreencorsa.module.scss"
 
 
-export default function VideoPlayer() {
 
-  return (
+const isSafari = () => {
+  const ua = navigator.userAgent.toLowerCase();
+  return ua.indexOf("safari") > -1 && ua.indexOf("chrome") < 0;
+};
+
+export default function App() {
+  const videoParentRef = useRef();
+  const [shouldUseImage, setShouldUseImage] = useState(false);
+  useEffect(() => {
+    // check if user agent is safari and we have the ref to the container <div />
+    if (isSafari()) {
+        setShouldUseImage(true);
+    }
+  }, []);
+
+  return shouldUseImage ? (
+    <img src={gifvideo}
+    style="position: relative;
+    left: 8%;
+    top: 18px;
+    height: 532px;
+    border-radius: 20px;
+    width: auto;
+    z-index: 8;
+    @media (max-width: 1158px) {
+        bottom: 410px;
+    }" alt="Muted Video" />
+  ) : (
     <div
+      ref={videoParentRef}
       dangerouslySetInnerHTML={{
         __html: `
         <video
