d517172a0c419f23d94ff80da727290c2d5922c4
Salim Bouhorma <67782664+bouhormq@users.noreply.github.com>
2021-02-02 14:20:07 +0100

    Update phonescreenraco.js

--- src/components/phonescreenraco.js
+++ src/components/phonescreenraco.js
@@ -5,6 +5,7 @@ import srcvideo from "../media/video2.gif"
 
 export default function PhoneScreenCorsa(props){
     return <div className = {PhoneScreenStyles.phone}>
+            <img className = {PhoneScreenStyles.mockphone} src ={require('../media/phone.png')} alt = " "></img>
             <img className = {PhoneScreenStyles.appvideo} src = {srcvideo}  alt = " "></img>
           </div>
 }
