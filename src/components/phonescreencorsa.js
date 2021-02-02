37698900e271eadfadc8c7b58be84f25c3723955
bouhormq <bouhormq@gmail.com>
2021-02-02 21:32:08 +0100

    mp4

--- src/components/phonescreencorsa.js
+++ src/components/phonescreencorsa.js
@@ -6,7 +6,7 @@ import srcvideo from "../media/corsa.mp4"
 export default function PhoneScreenCorsa(props){
     return <div className = {PhoneScreenStyles.phone}>
                 <img className = {PhoneScreenStyles.mockphone} src ={require('../media/phone.png')} alt = " "></img>
-                <video className = {PhoneScreenStyles.appvideo} loop autoPlay muted>
+                <video className = {PhoneScreenStyles.appvideo} playsinline loop autoPlay muted>
                     <source src={srcvideo} type="video/mp4" />
                 </video>            
             </div>
